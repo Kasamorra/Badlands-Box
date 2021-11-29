77dcfc34402b4952dd0d4b8946e2b1f8c0260c2a
Kasamorra <81055522+Kasamorra@users.noreply.github.com>
2021-11-29 18:22:04 +0100

    Add files via upload

---
+++ FINAL_CODE_BadlandsBox.ino
@@ -0,0 +1,88 @@
+//Music box that plays the melody of Bruce Springsteen's "Badlands" when opened and stops when close
+//Compoments: Arduino, passive buzzer, light dependent resistor,10k Ohm resistor 
+//Pins used: A0, 7
+
+#include "pitches.h" //file with all notes buzzer could play
+
+//Light Dependent Resistor
+const int ldrPin = A0;
+int lightLevel; //light value the ldr picks up
+const int bright = 450; //guideline value for when the box is opened
+const int dark = 300; //guideline value for when the box is closed
+
+//Passive Buzzer
+const int buzzPin = 7;
+
+// Array with Notes of the melody
+int16_t melody[] = {
+NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_DS5, NOTE_CS5, 
+NOTE_B4, NOTE_B4, NOTE_B4, NOTE_CS5, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_GS4, NOTE_A4, NOTE_A4, 
+NOTE_A4, NOTE_A4, NOTE_GS4, NOTE_FS4, NOTE_E4, NO_SOUND, NOTE_DS5, NOTE_E5, NOTE_E5, NOTE_E5, 
+NOTE_DS5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_DS5, 
+NOTE_CS5, NOTE_B4, NOTE_GS4, NOTE_B4, NOTE_E5, NOTE_DS5, NOTE_CS5, NOTE_B4, NOTE_E5, NOTE_E5, 
+NOTE_E5, NOTE_DS5, NOTE_CS5, NOTE_B4, NOTE_E5, NOTE_E5,
+}; //56 in total
+
+//Each tone's duration
+//Full Note = 1, Half Note = 2, ...
+int16_t noteDuration[] = {
+4, 3, 8, 8, 8, 8, 8, 8, 8, 8, 
+8, 8, 8, 8, 8, 4, 4, 8, 8, 8, 
+8, 8, 8, 8, 8, 8, 8, 16, 16, 16, 
+16, 8, 8, 4, 8, 8, 6, 16, 8, 8, 
+6, 16, 8, 2, 4, 4, 4, 4, 4, 2, 
+4, 4, 4, 4, 2, 1
+}; //56 in total
+
+
+const int songPace = 1450; //duration of full note in milliseconds
+int melodyLength = 0; 
+int noteLength; //how long a note is actually to be played
+const int breakTime = 300;
+int playState = 0; // 0 = waiting to play, 1 = Song playing, -1 = Song over
+int noteCount = 0; //Counter for position in array
+
+void setup() {
+  Serial.begin(9600); //for troubleshooting
+  pinMode(buzzPin, OUTPUT);
+
+  melodyLength = sizeof(melody) / sizeof(melody[0]); //calculates length of melody
+}
+
+void loop() {
+  lightLevel = analogRead(ldrPin); //ldr checks the light level
+  Serial.println(lightLevel); //for troubleshooting
+
+  if (lightLevel >= bright && playState == 0) { //checks if box opened and song not playing
+    playState = 1;  //changes state to "playing"
+  }
+  else if (lightLevel <= dark) //checks if box closed
+  {
+    if ((playState == 1) || (playState == -1)) //checks if melody playing or already over
+    {
+      playState = 0; //changes state to "song waiting to play"
+      noteCount = 0; //resets counter for array position
+    }
+  }
+
+  if (playState == 1) //checks if state is "playing"
+  {
+    noteLength = songPace / noteDuration[noteCount]; //calculates how long next note is to be played
+    
+    tone(buzzPin, melody[noteCount], noteLength); //plays next note
+    delay(noteLength*1.2);
+    noteCount = noteCount + 1; //updates counter
+
+    if (noteCount == melodyLength) //checks if full melody was played
+    {
+      noteCount = 0; //resets counter
+      playState = -1; //sets state to "song over"
+    }
+  }
+  else
+  {
+    digitalWrite(buzzPin, LOW); //turns of buzzer
+    noteCount = 0; //resets counter
+  }
+
+}
